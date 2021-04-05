a8f1b87643cf9ff8f90fa1bfda603291f37df8f5
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-05 23:24:45 +0530

    commit

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/manjesh23/paginateit',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
 
