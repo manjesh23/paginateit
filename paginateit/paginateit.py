d11ba5cdf23201008c5ee65c275431ac36dfba9c
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-05 23:08:50 +0530

    commit

--- paginateit/paginateit.py
+++ paginateit/paginateit.py
@@ -2,14 +2,15 @@
 
 # Paginateit
 
+
 def page(count, max_workers=2):
     ndiv = int(count/max_workers)
     sk = 0
     lm = ndiv
     for mw in range(1, (max_workers+2)):
-            globals()[f"skip{mw}"] = sk
-            globals()[f"limit{mw}"] = lm
-            globals()[f"flimit{mw}"] = str(lm)[1:].lstrip("0")
-            globals()[f"count{mw}"] = lm - sk
-            sk = sk + ndiv
-            lm = lm + ndiv
+        globals()[f"skip{mw}"] = sk
+        globals()[f"limit{mw}"] = lm
+        globals()[f"flimit{mw}"] = str(lm)[1:].lstrip("0")
+        globals()[f"count{mw}"] = lm - sk
+        sk = sk + ndiv
+        lm = lm + ndiv
