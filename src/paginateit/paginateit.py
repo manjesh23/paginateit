b60ef05742ff15329370f007e3b1c3aa9c45800a
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-06 23:15:51 +0530

    Updated examples and commit

--- src/paginateit/paginateit.py
+++ src/paginateit/paginateit.py
@@ -2,13 +2,27 @@
 
 # Paginateit
 
-# Adding notes in a while
+''' Module help us get the right values while creating API calls using pagination method.
+
+
+pg.page(1700, 8) --> Max records availabe in all pages is 1700 and are creating 8 threads / loops (We see skip[1-9] values)
+pg.page(90005, 6) --> Max records availabe in all pages is 90005 and are creating 6 threads / loops (We see skip[1-7] values)
+pg.page(5025) --> Max records availabe in all pages is 1700 and are creating 8 threads /loops (We see skip[1-3] values)
+
+pg.page(count, max_workers) # max_workers defaults to 2 unless specified
+
+--> Access the dynamic calculated variables as shown below:
+
+pg.skip1, pg.skip3, pg.limit2, pg.flimit4, pg.count2
+
+'''
 
 
 def page(count, max_workers=2):
     ndiv = int(count/max_workers)
     sk = 0
     lm = ndiv
+    # Loop based on count and calcuates the end values required for pagination
     for mw in range(1, (max_workers+2)):
         globals()[f"skip{mw}"] = sk
         globals()[f"limit{mw}"] = lm
