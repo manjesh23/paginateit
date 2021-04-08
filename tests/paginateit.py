80d55106f56c6ffd851e91d992ad302eea98aaac
Manjesh Setty <manjesh_n@hotmail.com>
2021-04-09 01:07:23 +0530

    Setup test

---
+++ tests/paginateit.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+def page(count, max_workers=2):
+    ndiv = int(count/max_workers)
+    sk = 0
+    lm = ndiv
+    # Loop based on count and calcuates the end values required for pagination
+    for mw in range(1, (max_workers+2)):
+        globals()[f"skip{mw}"] = sk
+        globals()[f"limit{mw}"] = lm
+        globals()[f"flimit{mw}"] = str(lm)[1:].lstrip("0")
+        globals()[f"count{mw}"] = lm - sk
+        sk = sk + ndiv
+        lm = lm + ndiv
+
+
+def test_page():
+    page(23052, 4)
+    assert limit5 == 28815
+    assert skip1 == 0
+    assert count4 == 5763
+    assert flimit3 == '7289'
